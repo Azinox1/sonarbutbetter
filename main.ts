e90e5509164ea9e38ee9293d217b8ed317e18a45
Azinox1 <93282160+Azinox1@users.noreply.github.com>
2021-11-05 11:54:27 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (distance > 60) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (distance <= 60 && distance > 50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+    if (distance <= 50 && distance > 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (distance <= 40 && distance > 30) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (distance <= 30 && distance > 20) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (distance <= 20) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
